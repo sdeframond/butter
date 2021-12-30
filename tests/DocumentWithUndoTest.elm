8dd06486b0ee7f2604fc31c82e743545efe42ed0
Samuel DE FRAMOND <samuel@framond.fr>
2021-12-30 19:06:26 +0100

    Fix test

--- tests/DocumentWithUndoTest.elm
+++ tests/DocumentWithUndoTest.elm
@@ -7,12 +7,8 @@ import Sheet
 import Test exposing (..)
 
 
-expectPastToChangeBy : Int -> (Document.Model -> Document.Model) -> Expect.Expectation
-expectPastToChangeBy delta func =
-    let
-        doc =
-            Document.init
-    in
+expectPastToChangeBy : Int -> (Document.Model -> Document.Model) -> Document.Model -> Expect.Expectation
+expectPastToChangeBy delta func doc =
     func doc
         |> (.past >> List.length)
         |> Expect.equal (List.length doc.past + delta)
@@ -20,47 +16,50 @@ expectPastToChangeBy delta func =
 
 suite : Test
 suite =
+    let
+        doc =
+            Document.init
+    in
     describe "DocumentWithUndo"
         [ test ".commitEditedSheetNames pushes a new state when there is a change" <|
             \_ ->
-                expectPastToChangeBy 1
-                    (Document.updateCurrentEditedSheetName "changed" >> Document.commitEditedSheetNames)
+                doc
+                    |> expectPastToChangeBy 1
+                        (Document.updateCurrentEditedSheetName "changed" >> Document.commitEditedSheetNames)
         , test ".commitEditedSheetNames does not push a new state when there is no change" <|
             \_ ->
-                expectPastToChangeBy 0
-                    (Document.updateCurrentEditedSheetName "not valid" >> Document.commitEditedSheetNames)
+                doc
+                    |> expectPastToChangeBy 0
+                        (Document.updateCurrentEditedSheetName "not valid" >> Document.commitEditedSheetNames)
         , test ".editCurrentSheetName does not push a new state" <|
             \_ ->
-                expectPastToChangeBy 0 Document.editCurrentSheetName
+                doc |> expectPastToChangeBy 0 Document.editCurrentSheetName
         , test ".insertSheet pushes a new state" <|
             \_ ->
-                expectPastToChangeBy 1 (Document.insertSheet Sheet.allParams.grid)
+                doc |> expectPastToChangeBy 1 (Document.insertSheet Sheet.allParams.grid)
         , test ".merge does not push a new state" <|
             \_ ->
-                expectPastToChangeBy 0 (Document.merge Document.init)
-        , test ".removeSheet pushes a new state" <|
+                doc |> expectPastToChangeBy 0 (Document.merge Document.init)
+        , test ".removeSheet does not push a new state when there is only one sheet left" <|
             \_ ->
-                expectPastToChangeBy 1 (\d -> Document.removeSheet (Document.getCurrentSheetId d) d)
+                doc |> expectPastToChangeBy 0 (\d -> Document.removeSheet (Document.getCurrentSheetId d) d)
+        , test ".removeSheet pushes a new state when there is more than one sheet" <|
+            \_ ->
+                doc
+                    |> Document.insertSheet Sheet.allParams.grid
+                    |> expectPastToChangeBy 1 (\d -> Document.removeSheet (Document.getCurrentSheetId d) d)
         , test ".updateCurrentEditedSheetName does not push a new state" <|
             \_ ->
-                expectPastToChangeBy 0 (Document.updateCurrentEditedSheetName "changed")
+                doc |> expectPastToChangeBy 0 (Document.updateCurrentEditedSheetName "changed")
         , todo ".updateCurrentSheet"
         , test ".decoder can decode .encoder's output" <|
             \_ ->
-                let
-                    doc =
-                        Document.init
-                in
                 doc
                     |> Document.encode
                     |> Json.Decode.decodeValue Document.decoder
                     |> Expect.equal (Ok doc)
         , test ".fromBytes can decode .toBytes's output" <|
             \_ ->
-                let
-                    doc =
-                        Document.init
-                in
                 doc
                     |> Document.toBytes
                     |> Document.fromBytes
